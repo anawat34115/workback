f811a8aac943fbd0c0a3ba12c68550005583ebc2
apex <anawat.apac@gmail.com>
2023-05-03 21:20:38 +0700

    first commit

--- server/index.js
+++ server/index.js
@@ -1,37 +1,24 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
-var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
-var jwt = require('jsonwebtoken');
-const secret = 'Fullstack-Login-2021'
-const saltRounds = 10
+const express = require('express');
+const mysql = require('mysql');
+const app = express()
 
-app.use(cors())
-
-const mysql = require('mysql2');
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'mydb'
+  host: 'localhost',
+  user: 'root',
+  database: 'formgym'
+});
+
+app.post('/submit', (req, res) => {
+  const formData = req.body;
+  const sql = 'INSERT INTO usres (fname, email, text) VALUES (?, ?, ?)';
+  connection.query(sql, [formData.fname, formData.email, formData.text], (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send('Form data saved successfully');
   });
+});
 
-app.post('/contact', jsonParser, function (req, res) {
-connection.execute(
-    'INSERT INTO user (fname, email, text ) VALUES (?, ?, ?)',
-    [req.body.fname, req.body.email, req.body.text],
-    function(err, results, fields) {
-        if (err) {
-            res.json({status:'eror', message: err})
-            return
-        }
-      res.json({status: 'ok'})
-    }
-  );
-})
-
-
-    app.listen(3333, function () {
-  console.log('Example app listening on port 3333')
-})
-    
+// Start the server
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
